cd84d4377eda8cb0c3cf5c4c18d7c2e6981501e2
root <root@DESKTOP-2MJ007O.localdomain>
2022-02-08 22:11:04 +0300

    finish map insert and map iterator

--- Map.hpp
+++ Map.hpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:53:58 by root              #+#    #+#             */
-/*   Updated: 2022/02/03 15:02:37 by root             ###   ########.fr       */
+/*   Updated: 2022/02/08 19:55:20 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ template <
 		typename Key,
 		typename T,
 		typename Compare = std::less<Key>,
-		typename Allocator = std::allocator< std::pair<const Key, T> >
+		typename Allocator = std::allocator< ft::pair<const Key, T> >
 		>
 class map {
 
@@ -79,20 +79,22 @@ class map {
 		//	Member fuctions
 
 		map() :
+			_size(0),
 			_comp(),
 			_node_alloc(),
 			_root(nullptr),
-			_nil(_node_alloc().allocate) {
-				__node_init(_nil, nullptr, _nil->left);
+			_nil(_node_alloc.allocate(1)) {
+				__node_init(_nil);
 			}
 
 		explicit map(const Compare &comp,
 					const Allocator &alloc = Allocator()) :
+						_size(0),
 						_comp(comp),
 						_node_alloc(alloc),
 						_root(nullptr),
-						_nil(_node_alloc().allocate) {
-							__node_init(_nil, nullptr, _nil->left);
+						_nil(_node_alloc.allocate(1)) {
+							__node_init(_nil);
 						}
 
 		template <typename InputIt>
@@ -100,25 +102,28 @@ class map {
 			InputIt last,
 			const Compare &comp = Compare(),
 			const Allocator &alloc = Allocator()) :
+				_size(0),
 				_comp(comp),
 				_node_alloc(alloc),
 				_root(nullptr),
-				_nil(_node_alloc().allocate) {
-					__node_init(_nil, nullptr, _nil->left);
+				_nil(_node_alloc.allocate(1)) {
+					__node_init(_nil);
 					insert(first, last);
 				}
 
 		map(const map &other) :
+			_size(0),
 			_comp(other._comp),
 			_node_alloc(other._node_alloc),
 			_root(nullptr),
-			_nil(_node_alloc().allocate) {
-				__node_init(_nil, nullptr, _nil->left);
+			_nil(_node_alloc.allocate(1)) {
+				__node_init(_nil);
 				insert(other.begin(), other.end());
 			}
 
 		~map() {
-			__prev_iter(_root, __node_destroy());
+			if (!empty())
+				__prev_iter(_root, &map::__node_destroy);
 			__node_destroy(_nil);
 		}
 
@@ -153,19 +158,19 @@ class map {
 
 		//	Iterators
 
-		iterator		begin() {
+		iterator				begin() {
 			return iterator(_last_left);
 		}
 		
-		const_iterator	begin() const {
+		const_iterator			begin() const {
 			return const_iterator(_last_left);
 		}
 
-		iterator		end() {
+		iterator				end() {
 			return iterator(_nil);
 		}
 
-		const_iterator	end() const {
+		const_iterator			end() const {
 			return const_iterator(_nil);
 		}
 
@@ -187,11 +192,11 @@ class map {
 
 		//	Capacity
 
-		bool		empty() const { return !_size; }
+		bool					empty() const { return !_size; }
 
-		size_type	size() const { return _size; }
+		size_type				size() const { return _size; }
 
-		size_type	max_size() const { }
+		size_type				max_size() const { }
 
 		//	Modifiers
 
@@ -203,8 +208,17 @@ class map {
 			_node_pointer new_node = __node_create(value);
 			_node_pointer returned_node = __insert(new_node);
 
-			if (returned_node == new_node)
+			// std::cout << "=====================" << std::endl;
+			// std::cout << new_node << std::endl;
+			// std::cout << new_node->parent << std::endl;
+			// std::cout << new_node->parent->left << std::endl;
+			// std::cout << new_node->parent->right << std::endl;
+			// std::cout << "=====================" << std::endl;
+
+			if (returned_node == new_node) {
+				++_size;
 				return ft::make_pair(iterator(new_node), true);
+			}
 			else {
 				__node_destroy(new_node);
 				return ft::make_pair(iterator(returned_node), false);
@@ -213,38 +227,55 @@ class map {
 
 		iterator					insert(iterator hint, const_reference value) {
 			(void)hint;
-			return insert(value).iterator;
+			return insert(value).first;
 		}
 
 		template <typename InputIt>
-		void	insert(InputIt first, InputIt last) {
+		void						insert(InputIt first, InputIt last) {
 			while (first != last)
 				insert(*first++);
 		}
 
+		void	erase(iterator pos) {
+			
+			__node_destroy(pos._M_node);
+		}
+
+		void	erase(iterator first, iterator last) {
+			while (first != last)
+				erase(first);
+		}
+
 	private :
+
+		_node_pointer	__erase(_node_pointer node) {
+
+		}
 
 		_node_pointer	__insert(_node_pointer new_node) {
 			if (empty()) {
-				_root = __node_init(new_node, nullptr, new_node->left);
+				_root = __node_init(new_node, _nil, new_node->left);
 				_last_left = _root;
 				_last_right = _root;
 				_nil->parent = _last_right;
 				return _root;
 			}
 			else {
-				_node_pointer	parent = __iter_comp(_root, new_node->value.first);
-				if (_comp(parent->value.first, new_node->value.first)) {
-					if (parent == _last_left)
-						_last_left = new_node;
-					return __node_init(new_node, parent, parent->left);
-				}
-				else if (_comp(new_node->value.first, parent->value.first)) {
+				_node_pointer	parent = __iter_comp(_root, new_node->value->first);
+				if (_comp(parent->value->first, new_node->value->first)) {
 					if (parent == _last_right) {
 						_last_right = new_node;
 						_nil->parent = _last_right;
+						_nil->left = _last_right;
 					}
 					return __node_init(new_node, parent, parent->right);
+				}
+				else if (_comp(new_node->value->first, parent->value->first)) {
+					if (parent == _last_left) {
+						_last_left = new_node;
+						_nil->right = _last_left;
+					}
+					return __node_init(new_node, parent, parent->left);
 				}
 				return parent;
 			}
@@ -257,29 +288,39 @@ class map {
 		}
 
 		_node_pointer	__iter_comp(_node_pointer p, key_type key) {
-			if (_comp(p->value.first, key)) {
+			if (_comp(key, p->value->first)) {
 				if (p->left != _nil)
 					return __iter_comp(p->left, key);
 			}
-			else if (p->right != _nil)
-				return __iter_comp(p->right, key);
+			else if (_comp(p->value->first, key)) {
+				if (p->right != _nil)
+					return __iter_comp(p->right, key);
+			}
 			return p;
 		}
 
-		void			__prev_iter(_node_pointer node, void (*func)(_node_pointer)) {
+		void			__prev_iter(_node_pointer node, void (map::*func)(_node_pointer)) {
 			if (node == _nil)
 				return ;
 			__prev_iter(node->left, func);
 			__prev_iter(node->right, func);
-			func(node);
+			(this->*func)(node);
 		}
 		
-		void			__post_iter(_node_pointer node, void (*func)(_node_pointer)) {
+		void			__post_iter(_node_pointer node, void (map::*func)(_node_pointer)) {
 			if (node == _nil)
 				return ;
-			func(node);
+			(this->*func)(node);
 			__post_iter(node->left, func);
 			__post_iter(node->right, func);
+		}
+
+		_node_pointer	__node_init(_node_pointer nil) {
+			nil->parent = nullptr;
+			nil->value = nullptr;
+			nil->left = nullptr;
+			nil->right = nullptr;
+			return _nil;
 		}
 
 		_node_pointer	__node_init(_node_pointer new_node, _node_pointer parent, _node_pointer &parent_direction) {
@@ -295,12 +336,13 @@ class map {
 			_node_alloc.construct(new_node);
 			new_node->value = _alloc.allocate(1);
 			_alloc.construct(new_node->value, value);
+			return new_node;
 		}
 
 		void			__node_destroy(_node_pointer elem) {
 			_alloc.destroy(elem->value);
 			_alloc.deallocate(elem->value, 1);
-			_node_alloc.destory(elem);
+			_node_alloc.destroy(elem);
 			_node_alloc.deallocate(elem, 1);
 		}
 
--- iterators/MapIterator.hpp
+++ iterators/MapIterator.hpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 14:04:14 by root              #+#    #+#             */
-/*   Updated: 2021/12/21 16:33:37 by root             ###   ########.fr       */
+/*   Updated: 2022/02/08 22:09:31 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,127 @@
 # define MAP_ITERATOR_HPP
 
 # include <iterator>
+# include "../utils.hpp"
+# include "../Map.hpp"
+
 
 namespace ft {
 
 
-template <typename T>
-class Node {
-
-	T		value;
-	Node	*parent;
-	Node	*right;
-	Node	*left;
-
-} ;
-
-template <typename T>
-class map_iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+template <typename Key, typename T>
+class binary_tree_iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
 	
 	private :
 
-		typedef	Node<T>		_node;
-		typedef	_node*		_node_pointer;
+		typedef ft::binary_node< ft::pair<Key, T> >	_node;
+		typedef _node*								_node_pointer;
+		typedef _node&								_node_reference;
 
-		_node_pointer	_p;
+		# define LAST value
+		# define LAST_PARENT parent->value
+		# define LAST_LEFT left->value
+		# define LAST_RIGHT right->value
 
 	public :
 
-		map_iterator() : _p(nullptr) { }
+		typedef typename _node::value_type		value_type;
+		typedef typename _node::value_type*		pointer;
+		typedef typename _node::value_type&		reference;
+		typedef std::ptrdiff_t					difference_type;
+		typedef std::bidirectional_iterator_tag	iterator_category;
 
-		map_iterator(const value_type p) : _p(p) { }
+		binary_tree_iterator() :
+			_M_node(nullptr) { }
 
-		~map_iterator() { }
+		binary_tree_iterator(const _node_pointer p) :
+			_M_node(p) { }
 
-		map_iterator	&operator = (const map_iterator &other) {
-			this->_p = other.base();
+		binary_tree_iterator(const binary_tree_iterator &other) :
+			_M_node(other._M_node) { }
+
+		~binary_tree_iterator() { }
+
+		const binary_tree_iterator	&operator = (const binary_tree_iterator &other) {
+			this->_M_node = other._M_node;
+			return *this;
 		}
 
-		reference		operator * () { return _p->value; }
+		reference		operator * () { return *_M_node->value; }
 
-		pointer			operator -> () { return &_p->value; }
+		pointer			operator -> () { return _M_node->value; }
+
+		binary_tree_iterator	&operator ++ () {
+			this->_M_node = __tree_increment(this->_M_node);
+			return *this;
+		}
+
+		binary_tree_iterator	operator ++ (int) {
+			binary_tree_iterator	dst = *this;
+			this->_M_node = __tree_increment(this->_M_node);
+			return dst;
+		}
+
+		binary_tree_iterator	&operator -- () {
+			this->_M_node = __tree_decrement(this->_M_node);
+			return *this;
+		}
+
+		binary_tree_iterator	operator -- (int) {
+			binary_tree_iterator	dst = *this;
+			this->_M_node = __tree_decrement(this->_M_node);
+			return dst;
+		}
+
+
+		bool		operator == (const binary_tree_iterator &other) const {
+			return this->_M_node == other._M_node;
+		}
+
+		bool		operator != (const binary_tree_iterator &other) const {
+			return this->_M_node != other._M_node;
+		}
+
+		
+		_node_pointer	_M_node;
 
 	private :
 
-		_node_pointer	_tree_increment(_node_pointer p) {
-			_node_pointer	prev = p;
-			if (p->right != nullptr)
-				return p;
-			while (p != nullptr) {
-				p = p->parent;
-				if (p->right != prev)
-			}
+		_node_pointer	__tree_increment(_node_pointer p) {
+			if (p->LAST != nullptr && p->LAST_RIGHT == nullptr)
+				return __tree_up_to_first_right(p, p->right);
+			else
+				return __tree_down_to_last_left(p->right);
+		}
+
+		_node_pointer	__tree_decrement(_node_pointer p) {
+			if (p->LAST != nullptr && p->LAST_LEFT == nullptr)
+				return __tree_up_to_first_left(p, p->left);
+			else
+				return __tree_down_to_last_right(p->left);
+		}
+
+		_node_pointer	__tree_up_to_first_right(_node_pointer pres, _node_pointer prev) {
+			if (pres->right == prev)
+				return __tree_up_to_first_right(pres->parent, pres);
+			return pres;
+		}
+
+		_node_pointer	__tree_up_to_first_left(_node_pointer pres, _node_pointer prev) {
+			if (pres->left == prev)
+				return __tree_up_to_first_left(pres->parent, pres);
+			return pres;
+		}
+
+		_node_pointer	__tree_down_to_last_left(_node_pointer p) {
+			if (p->LAST_LEFT != nullptr)
+				return __tree_down_to_last_left(p->left);
+			return p;
+		}
+
+		_node_pointer	__tree_down_to_last_right(_node_pointer p) {
+			if (p->LAST_RIGHT != nullptr)
+				return __tree_down_to_last_right(p->right);
+			return p;
 		}
 
 } ;
